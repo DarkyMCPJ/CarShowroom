ce67d8fb44856c632d524b33c6c9857e41058694
DarkyMCPJ <68011141@kmitl.ac.th>
2025-10-09 23:15:27 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,27 @@
+// main.c
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>   
+#include <termios.h>  
 #include "car.h"
 #include "fileio.h"
-#include "tests.h" 
+#include "tests.h"
 #define RESET "\033[0m"
 #define GREEN "\033[32m"
 #define RED   "\033[31m"
 
+
+char get_single_char() {
+    struct termios oldt, newt;
+    char ch;
+    tcgetattr(STDIN_FILENO, &oldt); 
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO); 
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt); 
+    ch = getchar(); 
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt); 
+    return ch;
+}
 
 int main(void) {
     initFileIfMissing(FILENAME);
@@ -16,25 +31,22 @@ int main(void) {
         printf("\n===== Car Management System =====\n");
         printf("1. Add Car\n");
         printf("2. Display Cars\n");
-        printf("3. Search Car (by Model or Year)\n");
+        printf("3. Search Car\n");
         printf("4. Update Car\n");
         printf("5. Delete Car\n");
- 
         printf("---------- TESTING SUITES ----------\n");
-        printf("6. Enter Test Arena (Sandbox Mode)\n"); 
+        printf("6. Enter Test Arena (Sandbox Mode)\n");
         printf("7. Run Automated Unit Tests\n");
         printf("8. Run Full E2E Test\n");
         printf("------------------------------------\n");
         printf("9. Exit\n");
         printf("Choose an option: ");
 
-        char buf[16];
-        if (!fgets(buf, sizeof(buf), stdin)) {
-            break; 
-        }
-        choice = atoi(buf); 
+        char c = get_single_char();
+        printf("%c\n", c); 
+        
 
-        // =======================================================
+        choice = c - '0';
 
         switch (choice) {
             case 1: addCarInteractive(FILENAME); break;
@@ -42,20 +54,17 @@ int main(void) {
             case 3: searchCarInteractive(FILENAME); break;
             case 4: updateCarInteractive(FILENAME); break;
             case 5: deleteCarInteractive(FILENAME); break;
-            case 6: runTestArena(); break; 
+            case 6: runTestArena(); break;
             case 7: runUnitTests(); break;
             case 8: runE2ETests(); break;
             case 9: printf("Exiting...\n"); return 0;
-            default: 
+            default:
                 printf(RED "Invalid choice!\n" RESET);
-                break;  
+                break;
         }
 
-                if (choice != 9) {
-            printf("\nPress Enter to continue...");
-            
-            int c;
-            while ((c = getchar()) != '\n' && c != EOF);
-        }
+        printf("\nPress Enter to continue...");
+        int key;
+        while ((key = getchar()) != '\n' && key != EOF);
     }
 }
