e7e41cf3337ba5e253306a7af9be36b7bc1fdd6b
DarkyMCPJ <68011141@kmitl.ac.th>
2025-10-09 17:17:12 +0700

    Update fileio.h

--- fileio.h
+++ fileio.h
@@ -6,5 +6,6 @@
 void initFileIfMissing(const char *filename);
 int loadAllCars(const char *filename, Car **cars);
 void saveAllCars(const char *filename, Car *cars, int count);
+void copyFile(const char *src, const char *dest);
 
 #endif // FILEIO_H
