03a0a40112b830e1553e9466ebd8bb4043a110c7
DarkyMCPJ <68011141@kmitl.ac.th>
2025-10-09 21:11:08 +0700

    Update main.c

--- main.c
+++ main.c
@@ -2,12 +2,16 @@
 #include <stdlib.h>
 #include "car.h"
 #include "fileio.h"
-#include "tests.h"
+#include "tests.h" 
+#define RESET "\033[0m"
+#define GREEN "\033[32m"
+#define RED   "\033[31m"
+
 
 int main(void) {
     initFileIfMissing(FILENAME);
-
     int choice;
+
     for (;;) {
         printf("\n===== Car Management System =====\n");
         printf("1. Add Car\n");
@@ -15,16 +19,22 @@ int main(void) {
         printf("3. Search Car (by Model or Year)\n");
         printf("4. Update Car\n");
         printf("5. Delete Car\n");
-        printf("6. Run Unit Tests\n");    
-        printf("7. Run E2E Test\n");     
-        printf("8. Exit\n");
+ 
+        printf("---------- TESTING SUITES ----------\n");
+        printf("6. Enter Test Arena (Sandbox Mode)\n"); 
+        printf("7. Run Automated Unit Tests\n");
+        printf("8. Run Full E2E Test\n");
+        printf("------------------------------------\n");
+        printf("9. Exit\n");
         printf("Choose an option: ");
 
         char buf[16];
         if (!fgets(buf, sizeof(buf), stdin)) {
             break; 
         }
-        choice = atoi(buf);
+        choice = atoi(buf); 
+
+        // =======================================================
 
         switch (choice) {
             case 1: addCarInteractive(FILENAME); break;
@@ -32,10 +42,20 @@ int main(void) {
             case 3: searchCarInteractive(FILENAME); break;
             case 4: updateCarInteractive(FILENAME); break;
             case 5: deleteCarInteractive(FILENAME); break;
-            case 6: runUnitTests(); break;  
-            case 7: runE2ETests(); break;   
-            case 8: printf("Exiting...\n"); return 0;
-            default: printf("Invalid choice!\n");
+            case 6: runTestArena(); break; 
+            case 7: runUnitTests(); break;
+            case 8: runE2ETests(); break;
+            case 9: printf("Exiting...\n"); return 0;
+            default: 
+                printf(RED "Invalid choice!\n" RESET);
+                break;  
+        }
+
+                if (choice != 9) {
+            printf("\nPress Enter to continue...");
+            
+            int c;
+            while ((c = getchar()) != '\n' && c != EOF);
         }
     }
 }
