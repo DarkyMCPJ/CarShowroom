f1171544aff2e438bcab9455ee3577a9c708eab4
DarkyMCPJ <68011141@kmitl.ac.th>
2025-10-09 17:10:26 +0700

    Add files via upload

---
+++ fileio.h
@@ -0,0 +1,10 @@
+#ifndef FILEIO_H
+#define FILEIO_H
+
+#include "car.h"
+
+void initFileIfMissing(const char *filename);
+int loadAllCars(const char *filename, Car **cars);
+void saveAllCars(const char *filename, Car *cars, int count);
+
+#endif // FILEIO_H
